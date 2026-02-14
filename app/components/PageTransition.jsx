854cc97036e455fe2905899723aedcfe4de57651
UmairZakria <tomrichard872@gmail.com>
2026-02-14 09:46:23 +0500

    mobile optimizied

--- app/components/PageTransition.jsx
+++ app/components/PageTransition.jsx
@@ -9,7 +9,7 @@ export default function PageTransition({ children }) {
     <AnimatePresence mode="wait">
       <motion.div
         key={pathname}
-        initial={{ opacity: 0, y: 5 }}
+        initial={false}
         animate={{ opacity: 1, y: 0 }}
         // exit={{ opacity: 0, y: 5 }}
         transition={{ duration: 0.4, ease: "circOut" }}
