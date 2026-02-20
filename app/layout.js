9a4eb1b186f23f9b5777ef84dfcfb9678ae3df79
UmairZakria <tomrichard872@gmail.com>
2026-02-20 05:49:14 +0500

    ads added

--- app/layout.js
+++ app/layout.js
@@ -143,8 +143,8 @@ export default function RootLayout({ children }) {
           }}
         />
         <GoogleAnalytics />
-        <Script src="https://pl28735289.effectivegatecpm.com/19/39/9b/19399ba474c25737727a403288572c40.js" />
-        <Script src="https://pl28735344.effectivegatecpm.com/59/57/4a/59574a153ab37dd51e9c1fc6d4347223.js" />
+        {/* <Script src="https://pl28735289.effectivegatecpm.com/19/39/9b/19399ba474c25737727a403288572c40.js" /> */}
+        {/* <Script src="https://pl28735344.effectivegatecpm.com/59/57/4a/59574a153ab37dd51e9c1fc6d4347223.js" /> */}
         <AdminProvider>
           <AuthProvider>
             <SmoothScrollProvider>
