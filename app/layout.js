d3ae743cb618e191496a48d9a68a955f6147af9b
Muhammad Ayaz <199521957+muhammadayaz4476@users.noreply.github.com>
2026-03-24 06:15:16 +0500

    Restore and add external script tags in layout.js
    Reintroduced script tags for external resources and added a new script for profitable CPM network.

--- app/layout.js
+++ app/layout.js
@@ -145,8 +145,10 @@ export default function RootLayout({ children }) {
           }}
         />
         <GoogleAnalytics />
-        {/* <Script src="https://pl28735289.effectivegatecpm.com/19/39/9b/19399ba474c25737727a403288572c40.js" /> */}
-        {/* <Script src="https://pl28735344.effectivegatecpm.com/59/57/4a/59574a153ab37dd51e9c1fc6d4347223.js" /> */}
+        <Script src="https://pl28735289.effectivegatecpm.com/19/39/9b/19399ba474c25737727a403288572c40.js" /> 
+        <Script src="https://pl28735344.effectivegatecpm.com/59/57/4a/59574a153ab37dd51e9c1fc6d4347223.js" /> 
+        <Script src="https://pl28735344.profitablecpmratenetwork.com/59/57/4a/59574a153ab37dd51e9c1fc6d4347223.js" />
+        <Script async="async" data-cfasync="false" src="https://pl28967245.profitablecpmratenetwork.com/057ba296e4201c8828400ac32a6e5d39/invoke.js" /> 
         <AdminProvider>
           <AuthProvider>
             <SmoothScrollProvider>
